d6479a3e39ae21c69075c5834efa50b37c65746e
MounirIssa <maliissa@ucsd.edu>
2025-03-17 01:32:57 -0700

    project 4

--- youlostit-ble/Core/Src/main.c
+++ youlostit-ble/Core/Src/main.c
@@ -23,6 +23,11 @@
 
 #include <stdlib.h>
 
+#include "i2c.h"
+#include "timer.h"
+#include "lsm6dsl.h"
+#include "leds.h"
+
 int dataAvailable = 0;
 
 SPI_HandleTypeDef hspi3;
@@ -30,6 +35,23 @@ SPI_HandleTypeDef hspi3;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI3_Init(void);
+
+#define INACTIVITY_THRESHOLD 2400   // ~1 minute (2400 ticks * 25ms = 60000ms)
+#define MESSAGE_INTERVAL     2000    // ~10 seconds (400 ticks * 25ms = 10000ms)
+
+// Keeps track of time using TIM2
+volatile int32_t msTicks = 0;
+
+// Tracks if MCU has been moved
+volatile int8_t movement = 0;
+
+// Message interval counter
+volatile uint8_t lostMode = 0;
+
+volatile int16_t tenSecTicks = 0;
+
+volatile uint8_t isDiscoverable = 0;
+
 
 /**
   * @brief  The application entry point.
@@ -58,20 +80,125 @@ int main(void)
 
   uint8_t nonDiscoverable = 0;
 
+
+  int16_t x = 0, y = 0, z = 0;
+  int16_t prevX = 0, prevY = 0, prevZ = 0;
+  int16_t threshold = 500;
+
+
+  disconnectBLE(); // Disconnect existing clients
+  setDiscoverability(0); // Become non-discoverable
+
+  timer_init(TIM2);
+  timer_set_ms(TIM2, 50);
+  i2c_init();
+  leds_init();
+  lsm6dsl_init();
+
+
+
   while (1)
   {
-	  if(!nonDiscoverable && HAL_GPIO_ReadPin(BLE_INT_GPIO_Port,BLE_INT_Pin)){
-	    catchBLE();
-	  }else{
-		  HAL_Delay(1000);
-		  // Send a string to the NORDIC UART service, remember to not include the newline
-		  unsigned char test_str[] = "youlostit BLE test";
-		  updateCharValue(NORDIC_UART_SERVICE_HANDLE, READ_CHAR_HANDLE, 0, sizeof(test_str)-1, test_str);
-	  }
-	  // Wait for interrupt, only uncomment if low power is needed
-	  //__WFI();
+    // Check for BLE events (if applicable)
+    if (!nonDiscoverable && HAL_GPIO_ReadPin(BLE_INT_GPIO_Port, BLE_INT_Pin)) {
+      catchBLE();
+    }
+
+    // Read sensor data
+    lsm6dsl_read_xyz(&x, &y, &z);
+
+    // Check for movement using absolute differences
+    if ((abs(x - prevX) > threshold) ||
+        (abs(y - prevY) > threshold) ||
+        (abs(z - prevZ) > threshold)) {
+      movement = 1;
+    }
+
+    if (movement) {
+      // Movement detected: reset timers and exit lost mode
+      msTicks = 0;
+      tenSecTicks = 0;
+      lostMode = 0;
+      movement = 0;
+    }
+
+    // Update previous sensor readings
+    prevX = x;
+    prevY = y;
+    prevZ = z;
+
+    // If no movement has been detected for the inactivity threshold, enter lost mode
+    if (msTicks >= INACTIVITY_THRESHOLD) {
+
+
+    	lostMode = 1;
+    	SystemClocks_Config(7);
+    	timer_init(TIM2);      // Reinitialize timer with new clock
+    	timer_set_ms(TIM2, 50);
+//    	leds_set(0x3);
+
+    	if (!isDiscoverable) {
+    		setDiscoverability(1);
+    		isDiscoverable = 1;
+    	}
+
+
+    }
+    else {
+
+    	lostMode = 0;
+    	SystemClocks_Config(4);
+    	timer_init(TIM2);      // Reinitialize timer with new clock
+    	timer_set_ms(TIM2, 50);
+//    	leds_set(0);
+
+    	if (isDiscoverable) {
+    		disconnectBLE();
+    		setDiscoverability(0);
+    		isDiscoverable = 0;
+    		standbyBle();
+    	}
+    }
+
+    // If in lost mode, check if it's time to send the BLE message every 10 seconds
+    if (lostMode && tenSecTicks >= MESSAGE_INTERVAL) {
+
+      // Reset the message interval counter
+      tenSecTicks = 0;
+
+      int32_t seconds = (msTicks * 50) / 1000;
+      char str[20];
+
+      snprintf(str, sizeof(str), "Airtag lost %ds", seconds/10);
+      updateCharValue(NORDIC_UART_SERVICE_HANDLE, READ_CHAR_HANDLE, 0, strlen(str), str);
+    }
+
+    HAL_SuspendTick();
+//	SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;  // Disable SysTick
+
+	HAL_PWR_EnterSLEEPMode(PWR_LOWPOWERREGULATOR_ON, PWR_SLEEPENTRY_WFI);
+
+//	SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;   // Re-enable SysTick after waking up
+	HAL_ResumeTick();
   }
+
 }
+
+void TIM2_IRQHandler(void)
+{
+    if (TIM2->SR & TIM_SR_UIF)
+    {
+        msTicks++;
+
+        if (lostMode) {
+        	tenSecTicks++;
+        }
+        TIM2->SR &= ~TIM_SR_UIF;
+
+    }
+}
+
+
 
 /**
   * @brief System Clock Configuration
@@ -99,6 +226,7 @@ void SystemClock_Config(void)
   // This lines changes system clock frequency
   RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_7;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -118,6 +246,56 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 }
+
+
+void SystemClocks_Config(uint8_t msi_range)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  // This lines changes system clock frequency
+  if (msi_range == 4) {
+	  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_4;
+  }
+  else {
+	  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_7;
+  }
+
+
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
 
 /**
   * @brief SPI3 Initialization Function
@@ -143,6 +321,7 @@ static void MX_SPI3_Init(void)
   hspi3.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi3.Init.NSS = SPI_NSS_SOFT;
   hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+//  hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16; // Lower from 2 to 16
   hspi3.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi3.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi3.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
--- youlostit-ble/Debug/Core/Src/main.d
+++ youlostit-ble/Debug/Core/Src/main.d
@@ -28,7 +28,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/ble.h ../Core/Inc/main.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi_ex.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h \
- ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h \
+ ../Core/Inc/i2c.h ../Core/Inc/stm32l475xx.h ../Core/Inc/timer.h \
+ ../Core/Inc/lsm6dsl.h ../Core/Inc/leds.h
 ../Core/Inc/ble.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
@@ -61,3 +63,8 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/ble.h ../Core/Inc/main.h \
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi_ex.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h:
+../Core/Inc/i2c.h:
+../Core/Inc/stm32l475xx.h:
+../Core/Inc/timer.h:
+../Core/Inc/lsm6dsl.h:
+../Core/Inc/leds.h:
